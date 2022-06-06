e6a0ed40023f48f94686d1d03fe96e1c9b142189
Miloud Bagaa <72396190+miloudbagaa@users.noreply.github.com>
2022-06-06 15:20:55 +0300

    Update readme and consider the case a higher ceph version is installed instead. (#2)
    Update readme and consider the case a higher Ceph version is installed instead. 
    
    In the current version, the role shows all the VMs have been launched before a specific Ceph version. This means that those VMs don't have the patch applied after the specified version.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ansible-role-vms-before-ceph-client
 
-Ansible role in listing all the VMs launched before the upgrade or installing a Ceph client in hypervisors. This role uses the ceph_version variable to specify the exact version of a Ceph client. If the requested Ceph client version is not installed, then the role considers the latest version installed lower than the requested version ceph_version. The output of the role consists of two parts: i) The latest Ceph client version (i.e., ceph_latest_installed) that is lower or equal to ceph_version; ii) The list of VMs that have been launched before ceph_latest_installed, and hence they don't have the patch applied on ceph_latest_installed.
+Ansible role that lists all the VMs launched before the upgrade or installing a Ceph client in hypervisors. This role uses the ceph_version variable to specify the exact version of a Ceph client. If the requested Ceph client version is not installed, then the role considers the latest version installed lower than the requested version ceph_version. The output of the role consists of he list of VMs that have been launched before ceph_version is installed or updated, and hence they don't have the patch applied on ceph_version.
 
 Role Variables
 --------------
@@ -21,7 +21,11 @@ Example Playbook
   become: True
   gather_facts: False
   vars:
-    ceph_version: "12.2.14"
+    ceph_version: "14.2.20"
   roles:
     - { role: ansible-role-vms-before-ceph-client }
 ```
+
+Limitation
+----------
+The current version does not consider the downgrade or remove of Ceph clients in hypervisors.
--- files/vms-before-ceph-version.sh
+++ files/vms-before-ceph-version.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Managed with Ansible
+# Author: Miloud Bagaa
+
 
 # DEFAULTS
 # Search for lanched VMs before this Ceph version
@@ -11,6 +13,13 @@ version_le_op() {
     [  "$1" = "$(echo -e "$1\n$2" | sort -V | head -n1)" ]
 }
 
+# This function compares two versions and retrun True
+# if $1 is strictly lower than $2 
+version_lt_op() {
+    [ "$1" = "$2" ] && return 1 || version_le_op $1 $2
+}
+
+# This function shows the utilization parmeters
 usage() {
   echo "Usage: $0 "
   echo " [--help|-h|-?],  to show the help"
@@ -39,23 +48,65 @@ done
 
 YUM_HISTORY_EVENTS=$(yum history list all|grep -Po "^\s+\d+")
 
+# Loop on all the events list related to ceph-common
 for YUM_HISTORY_EVENT in $YUM_HISTORY_EVENTS; do
-  CEPH_VERSION=$(yum history info $YUM_HISTORY_EVENT|grep -A1 ceph-common|tail -1|awk -F':' '{print $2}'|awk -F'-' '{print $1}')
+  CEPH_INFO=$(yum history info $YUM_HISTORY_EVENT|grep ceph-common);
+
+  # Avoid all events, such as remove, except the install and updates of ceph-common   
+  if [[ ${CEPH_INFO} != *"Install"* ]] && [[ ${CEPH_INFO} != *"Updated"* ]]; then
+     continue;
+  fi
+
+  # Extract the ceph version in case of a fresh install.
+  # tail -1 is used here to ensure that we get the last line of ceph-common-2:xx.y.zz-0.el7.x86_64
+  if [[ ${CEPH_INFO} == *"Install"* ]]; then
+     CEPH_VERSION=$(yum history info $YUM_HISTORY_EVENT|grep ceph-common|tail -1|awk -F':' '{print $2}'|awk -F'-' '{print $1}')
+  fi
+
+  # Extract the ceph version in case of an update.
+  if [[ ${CEPH_INFO} == *"Updated"* ]]; then
+     CEPH_VERSION=$(yum history info $YUM_HISTORY_EVENT|grep -A1 ceph-common|tail -1|awk -F':' '{print $2}'|awk -F'-' '{print $1}')
+  fi
+
+  # Get the date and timestamp of ceph update or install.
+  CEPH_UPGRADE_DATE=$(yum history info $YUM_HISTORY_EVENT|grep '^Begin time'|awk -F' : ' '{print $2}')
+  CEPH_UPGRADE_TIMESTAMP=$(date -d "$CEPH_UPGRADE_DATE" +%s)
+
+  # Break in case that $CEPH_VERSION is lower or equal to $CEPH_VERSION_EXPECTED.
   if version_le_op $CEPH_VERSION $CEPH_VERSION_EXPECTED; then
-    CEPH_UPGRADE_DATE=$(yum history info $YUM_HISTORY_EVENT|grep '^Begin time'|awk -F' : ' '{print $2}')
-    CEPH_UPGRADE_TIMESTAMP=$(date -d "$CEPH_UPGRADE_DATE" +%s)
     break
   fi
+
+  # Save the previous information in case $CEPH_VERSION is strictly lower than $CEPH_VERSION_EXPECTED. 
+  # In this case, we have to use the previous $CEPH_VERSION in the loop that has higher timestamp either is higher than $CEPH_VERSION_EXPECTED.
+  PREVIOUS_CEPH_VERSION=$CEPH_VERSION
+  PREVIOUS_CEPH_UPGRADE_DATE=$CEPH_UPGRADE_DATE
+  PREVIOUS_CEPH_UPGRADE_TIMESTAMP=$CEPH_UPGRADE_TIMESTAMP
+
 done
 
+# In case that the ceph is not installed in the system. 
+# No prior install or update of ceph client
 if [[ -z "$CEPH_UPGRADE_TIMESTAMP" ]]; then
-    echo "There is no Ceph version prior $CEPH_VERSION_EXPECTED is installed" 1>&2
+    echo "Ceph is not installed in the system" 1>&2
     exit 0
 fi
 
-echo "+-----------------------------+"
-echo "|   CEPH VERSION IS $CEPH_VERSION   |"
-echo "+-----------------------------+"
+# In case that there is no prior ceph version than $CEPH_VERSION_EXPECTED. 
+# This means that the code has completed the previous loop without break.
+if version_lt_op $CEPH_VERSION_EXPECTED $CEPH_VERSION; then
+    echo "There is no Ceph version lower or equal to $CEPH_VERSION_EXPECTED is installed" 1>&2
+    exit 0
+fi
+
+# In case that $CEPH_VERSION is strictly lower than $CEPH_VERSION_EXPECTED, the previous $CEPH_VERSION in the loop that has higher timestamp either is higher than $CEPH_VERSION_EXPECTED should be used.
+if version_lt_op $CEPH_VERSION $CEPH_VERSION_EXPECTED; then
+  if [[ ${PREVIOUS_CEPH_UPGRADE_TIMESTAMP+x} ]]; then
+    CEPH_VERSION=$PREVIOUS_CEPH_VERSION
+    CEPH_UPGRADE_DATE=$PREVIOUS_CEPH_UPGRADE_DATE
+    CEPH_UPGRADE_TIMESTAMP=$PREVIOUS_CEPH_UPGRADE_TIMESTAMP
+  fi 
+fi
 
 # Retrieve a list of all QEMU process IDs
 PIDS=$(pgrep -f "^/usr/libexec/qemu-kvm")
